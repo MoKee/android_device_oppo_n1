8c49e2d0e81df655dbe6099b75b4f31ddbcdad7f
martincz <martincz@mokeedev.com>
2014-01-08 05:03:59 +0800

    update to mokee
    Change-Id: I7eba3f367c395b34f9afabf73210db932aa08377

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 - 2014 The MoKee OpenSource Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom lpj=67677 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 zcache
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
-TARGET_KERNEL_CONFIG := cyanogenmod_n1_defconfig
+TARGET_KERNEL_CONFIG := mokee_n1_defconfig
 TARGET_KERNEL_SOURCE := kernel/oppo/n1
 
 # Flags
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Device tree for Oppo N1 (CyanogenMod Edition!)
 
-Copyright 2014, The MoKee OpenSource Project
+Copyright 2013 - 2014, The MoKee OpenSource Project
 
